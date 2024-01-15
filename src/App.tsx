ce001ce19d8ac2e88dd2f49f6bc1b9f0dce710a7
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-01-16 00:46:41 +0900

    typescript study

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 /*eslint-disable*/
-import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
+import Todo from './types/todos';
+
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    })
+  }
+
+  const removeTodoHandler = (todoId : string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    })
+  }
+
   return (
-    <div className="App">
-      typescript test
+    <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
+      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
