8c6331e892361ca2ccd7be48196d4ad54dc975a1
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-01-18 21:23:27 +0900

    simple commit

--- src/components/ActivityDropDown.tsx
+++ src/components/ActivityDropDown.tsx
@@ -10,7 +10,18 @@ interface Point {
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  padding : 20px;
 `
+
+const Wrapper = styled.div`
+  //스타일 고민
+`
+
+const Selection = styled.div`
+  margin: 20px 0;
+`
+
 
 const ActivityDropDown: React.FC = () => {
   const [selectedArea, setSelectedArea] = useState<string | null>(null);
@@ -65,47 +76,54 @@ const ActivityDropDown: React.FC = () => {
 
   return (
     <Container>
-      <select value={selectedArea ?? ''} onChange={handleAreaChange}>
-        <option value="">영역을 선택해주세요</option>
-        {ActivityDropDownData.map((areaData, index) => (
-          <option key={index} value={areaData.area}>{areaData.area}</option>
-        ))}
-      </select>
-
-      {selectedArea && (
-        <select value={selectedProgram ?? ''} onChange={handleProgramChange}>
-          <option value="">프로그램을 선택해주세요</option>
-          {ActivityDropDownData.find(area => area.area === selectedArea)?.programs.map((program, index) => (
-            <option key={index} value={program.program}>{program.program}</option>
-          ))}
-        </select>
-      )}
-
-      {selectedProgram && (
-        <select value={selectedType ?? ''} onChange={handleTypeChange}>
-          <option value="">프로그램 종류를 선택해주세요</option>
-          {ActivityDropDownData.find(area => area.area === selectedArea)
-            ?.programs.find(program => program.program === selectedProgram)
-            ?.types.map((type, index) => (
-              <option key={index} value={type.type}>{type.type}</option>
-          ))}
-        </select>
-      )}
-
-      {selectedType && (
-        <select value={selectedTopic ?? ''} onChange={handleTopicChange}>
-          <option value="">주제를 선택해주세요</option>
-          {ActivityDropDownData.find(area => area.area === selectedArea)
-            ?.programs.find(program => program.program === selectedProgram)
-            ?.types.find(type => type.type === selectedType)
-            ?.points.map((point, index) => (
-              <option key={index} value={point.topic}>{point.topic}</option>
-          ))}
-        </select>
-      )}
-
-      {/* Optionally display the selected point */}
-      {selectedPoint !== null && <p>선택한 점수: {selectedPoint}</p>}
+      <Wrapper>
+        <Selection>
+          <select value={selectedArea ?? ''} onChange={handleAreaChange}>
+            <option value="">영역을 선택해주세요</option>
+            {ActivityDropDownData.map((areaData, index) => (
+              <option key={index} value={areaData.area}>{areaData.area}</option>
+            ))}
+          </select>
+        </Selection>
+        
+        <Selection>
+          {selectedArea && (
+            <select value={selectedProgram ?? ''} onChange={handleProgramChange}>
+              <option value="">프로그램을 선택해주세요</option>
+              {ActivityDropDownData.find(area => area.area === selectedArea)?.programs.map((program, index) => (
+                <option key={index} value={program.program}>{program.program}</option>
+              ))}
+            </select>
+          )}
+        </Selection>
+        
+        <Selection>
+          {selectedProgram && (
+            <select value={selectedType ?? ''} onChange={handleTypeChange}>
+              <option value="">프로그램 종류를 선택해주세요</option>
+              {ActivityDropDownData.find(area => area.area === selectedArea)
+                ?.programs.find(program => program.program === selectedProgram)
+                ?.types.map((type, index) => (
+                  <option key={index} value={type.type}>{type.type}</option>
+              ))}
+            </select>
+          )}
+        </Selection>
+        <Selection>
+          {selectedType && selectedTopic !== '' && (
+            <select value={selectedTopic ?? ''} onChange={handleTopicChange}>
+              <option value="">주제를 선택해주세요</option>
+              {ActivityDropDownData.find(area => area.area === selectedArea)
+                ?.programs.find(program => program.program === selectedProgram)
+                ?.types.find(type => type.type === selectedType)
+                ?.points.map((point, index) => (
+                  <option key={index} value={point.topic}>{point.topic}</option>
+              ))}
+            </select>
+          )}
+        </Selection>
+        {selectedPoint !== null && <p>선택한 점수: {selectedPoint}</p>}
+      </Wrapper>
     </Container>
   );
 };
