2a1a29984b46042b9f211b53ee2545fe9bffa6b8
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-01-26 19:29:45 +0900

    idea commit

--- src/pages/SWConvergencePage.tsx
+++ src/pages/SWConvergencePage.tsx
@@ -43,6 +43,10 @@ const SWConvergencePage:React.FC = () => {
 		
 	}, [activitiesData])
 
+	//저장 버튼을 만들거임. 그리고 이 저장버튼이 눌렸는지 안눌렸는지를 state로 관리해보면
+	// 어떨까 싶음. 5초마다 이 페이지 내용을 자동으로 백엔드로 보내주는 로직. 이때, 저장버튼이
+	//눌리지 않았다는 것이 확인되면 이 로직이 발동하고, 이미 눌려있다면 굳이 발동되지 않는다. 이렇게 구현해보자.
+
 	return (
 		<MainLayout>	
 			<div className={classes.button_container}>
