2f57e7dae20f98808da6f7fd2d5d994bed3a5852
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-01-30 14:00:01 +0900

    Update useGetDataFromBE.ts

--- src/hooks/useGetDataFromBE.ts
+++ src/hooks/useGetDataFromBE.ts
@@ -1,10 +1,26 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const useGetDataFromBE = (pageType: string) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<Error | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/zs/${pageType}`);
+        setData(response.data);
+      } catch (err) {
+        setError(err as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-const useGetDataFromBE = () => {
-	useEffect(()=> {
+    fetchData();
+  }, [pageType]);
 
-	}, [])
-}
+  return { data, loading };
+};
 
 export default useGetDataFromBE;
