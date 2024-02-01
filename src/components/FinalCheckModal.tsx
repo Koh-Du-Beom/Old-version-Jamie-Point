088480dea197e88804eb58e5415d3a7e6bd6635e
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-01 23:07:52 +0900

    modal finished

---
+++ src/components/FinalCheckModal.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useRef } from "react";
+import classes from '../styles/components/FinalCheckModal.module.css';
+import { useSelector, } from 'react-redux';
+import { RootState, } from "../stores/redux/store";
+import axios from 'axios';
+import saveIcon from '../assets/saveIcon.png';
+
+interface ModalProps{
+	onClose : () => void;
+}
+
+const FinalCheckModal : React.FC<ModalProps> = ( {onClose}) => {
+	const userInfo = useSelector((state : RootState) => state.userInfo);
+
+	const handleDocumentation = async() => {
+		
+		const body = JSON.stringify(userInfo);
+		console.log(body);
+
+		try{
+			const response = await axios.post('http://localhost:8080/zs', body, {
+				headers: {
+					'Content-Type' : 'application/json'
+				}
+			});
+			
+			if (response){
+				console.log(response.data);
+			}
+			alert("문서 변환이 성공적으로 완료되었습니다!");
+			onClose();
+		}catch(error){
+			console.error('문서화 실패 : ', error);
+			alert("문서 변환에 실패하였습니다!");
+			onClose();
+		}
+	}
+	
+	const modalRef = useRef<HTMLDivElement>(null);
+
+	useEffect(()=>{
+		const handleClickOutside = (event : MouseEvent) => {
+			if (modalRef.current && !modalRef.current.contains(event.target as Node)){
+				onClose();
+			}
+		};
+
+		document.body.style.overflow = 'hidden';
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.body.style.overflow = 'unset';
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [onClose]);
+
+	return (
+		<div>
+			<div className={classes.modalOverlay} onClick={onClose}>
+				<div className={classes.modalContent} onClick={e => e.stopPropagation()} ref={modalRef}>
+					<div className={classes.big_title}>내용을 확인해주세요!</div>
+					<div className="tableContainer">
+						<table>
+							<thead>
+								<tr>
+									<th></th>
+									<th>활동 수</th>
+									<th>활동 점수</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>SW핵심역량</td>
+									<td>{userInfo.swCoreInfo.activityCount}</td>
+									<td>{userInfo.swCoreInfo.totalPoint}</td>
+								</tr>
+								<tr>
+									<td>SW산학협력·창업역량</td>
+									<td>{userInfo.swCooperationInfo.activityCount}</td>
+									<td>{userInfo.swCooperationInfo.totalPoint}</td>
+								</tr>
+								<tr>
+									<td>SW가치확산역량</td>
+									<td>{userInfo.swValueInfo.activityCount}</td>
+									<td>{userInfo.swValueInfo.totalPoint}</td>
+								</tr>
+								<tr>
+									<td>SW융합역량</td>
+									<td>{userInfo.swConvergenceInfo.activityCount}</td>
+									<td>{userInfo.swConvergenceInfo.totalPoint}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<div className={classes.saveButtonContainer}>
+						<button className={classes.saveButton} onClick={handleDocumentation}>
+							<img src={saveIcon} alt="saveIcon"/>
+						</button>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	)
+};
+
+export default FinalCheckModal;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TopNavBar from '../components/TopNavBar';
 import RightNavBar from '../components/RightNavBar';
 import classes from '../styles/layouts/MainLayout.module.css';
 import BottomNavBar from '../components/Footer/BottomNavBar';
 import BottomInfo from '../components/Footer/BottomInfo';
+import FinalCheckModal from '../components/FinalCheckModal';
+
 
 const MainLayout: React.FC<{children : React.ReactNode}> = ({children}) => {
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const openModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
 
   return (
     <div className={classes.mainLayout}>    
@@ -16,9 +21,14 @@ const MainLayout: React.FC<{children : React.ReactNode}> = ({children}) => {
 					<RightNavBar/>
 					
 				</div>
+				{ isModalOpen ? <FinalCheckModal onClose={closeModal}/> : null }
+				
       </div>
-      <BottomNavBar/>
+			
+			<BottomNavBar openModal={openModal}/>
 			<BottomInfo/>
+			
+			
     </div>
   )
 }
