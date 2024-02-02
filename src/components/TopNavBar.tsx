91fadc70984565cd7fb82d5ad7679f8a8f8bd3f9
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-02 16:05:41 +0900

    RightNavBar animation add

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,8 +1,33 @@
 import classes from '../styles/components/TopNavBar.module.css';
 import React from 'react';
-const TopNavBar:React.FC = () => {
+
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const TopNavBar : React.FC = () => {
   return (
-    <div className={classes.navbar}>상단 고정바 컨텐츠 생각하기</div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Jamie-Point</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item>Action</NavDropdown.Item>
+              <NavDropdown.Item> Another action</NavDropdown.Item>
+              <NavDropdown.Item>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
