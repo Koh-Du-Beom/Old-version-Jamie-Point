2d305e08d577ed24d8671182774fcea5e93e5658
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-05 23:21:13 +0900

    validation added

---
+++ src/utils/regularExpression/isValidUserInfo.ts
@@ -0,0 +1,27 @@
+
+const isValidName =  (name : string) => {
+	const regex = /^[가-힣a-zA-Z]+$/;
+	return regex.test(name);
+}
+
+const isValidAccountNumber = (accountNumber : string) : boolean => {
+	const regex = /^[\d\s-]{8,16}$/;
+  return regex.test(accountNumber);
+}
+
+const isValidEmail = (email : string) : boolean => {
+	const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+	return regex.test(email)
+}
+
+const isValidPhoneNumber = (phoneNumber : string) : boolean => {
+	const regex = /^01[016789]-\d{3,4}-\d{4}$|^\d{2,3}-\d{3,4}-\d{4}$/;
+	return regex.test(phoneNumber);
+}
+
+const isValidStudentNumber = (studentNumber : string) : boolean => {
+	const regex = /^\d+$/;
+	return regex.test(studentNumber);
+}
+
+export {isValidName, isValidAccountNumber, isValidEmail, isValidPhoneNumber, isValidStudentNumber };
