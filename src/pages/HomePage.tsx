bef9fc0f64656d59fe6090c55c78fab6fd77599c
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-08 01:11:37 +0900

    homepage making

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,17 @@
 import HomeLayout from "../layouts/HomeLayout";
-
+import classes from '../styles/page/HomePage.module.css';
 
 const HomePage = () => {
   return (
     <HomeLayout>
-			콘텐츠 고민
+			<div className={classes.container}>
+				<div className={classes.wrapper}>
+					콘텐츠 고민1
+				</div>
+				<div className={classes.wrapper}>
+					콘텐츠고민 2
+				</div>
+			</div>
 		</HomeLayout>
   )
 }
