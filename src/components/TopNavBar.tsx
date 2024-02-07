c64eaab399d14f4a98bf9bbfa09236e14fd2120f
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-07 17:52:24 +0900

    logo added

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import classes from '../styles/components/TopNavBar.module.css';
+import JPointLogo from '../assets/JPointLogo2.webp';
 
 const TopNavBar : React.FC = () => {
 	const navigate = useNavigate();
@@ -8,7 +9,7 @@ const TopNavBar : React.FC = () => {
   return (
     <div className={classes.navbar}>
 			<div className={classes.logo} onClick={()=> navigate('/')}>
-				Logo
+				<img className={classes.logo} src={JPointLogo} alt='JPointLogo'/>
 			</div>
 			<div className={classes.navItems}>
 				<div className={classes.navItem}>1</div>
