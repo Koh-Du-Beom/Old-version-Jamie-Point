3b4fd17e6d0ceedf3000e083b2403cc89c22ab4d
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-07 22:58:46 +0900

    TopNavBar finished

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import classes from '../styles/components/TopNavBar.module.css';
 import JPointLogo from '../assets/JPointLogo2.webp';
-
+import userInfoLogo from '../assets/userInfo.webp';
 const TopNavBar : React.FC = () => {
 	const navigate = useNavigate();
 
   return (
     <div className={classes.navbar}>
 			<div className={classes.logo} onClick={()=> navigate('/')}>
-				<img className={classes.logo} src={JPointLogo} alt='JPointLogo'/>
+				<img src={JPointLogo} alt='JPointLogo'/>
 			</div>
 			<div className={classes.navItems}>
-				<div className={classes.navItem}>1</div>
-				<div className={classes.navItem}>2</div>
+				<div className={classes.navItem}>추후에 활동추가</div>
+				<div className={classes.navItem}>어떤내용할지 고민</div>
 			</div>
-			<div className={classes.navIcons}>
-				<div className={classes.navIcon} onClick={()=>navigate('/info')}>유저 정보</div>
-				<div className={classes.navIcon} onClick={()=>navigate('/activity/SW핵심역량')}>활동</div>
-				<div className={classes.navIcon}>5</div>
+			<div className={classes.navItems}>
+				
+				<div className={classes.navItem} onClick={()=>navigate('/activity/SW핵심역량')}>활동 입력</div>
+				<div className={classes.navItem} onClick={()=>navigate('/info')}>
+					<img src={userInfoLogo} alt='userInfoLogo'/>
+				</div>
 			</div>
 		</div>
   );
